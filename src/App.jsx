6c7de16c159c9f8f1e8ee7f2725eca969d651b7b
YANGDAYOUNG <jc1525@hanmail.net>
2022-11-08 15:39:11 +0900

    #4 feat: 여러 작업들..

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePages from './pages/HomePages'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<HomePages />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
