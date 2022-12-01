4a2ce528e6ec843c4ce37fa12fa5b0941cb1db60
Yerin <zlekfl0311@gmail.com>
2022-12-01 22:06:33 +0900

    #4 feat: App.jsx 라우터 수정, 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePages from './pages/HomePages'
 import LoginPages from './pages/LoginPages'
+import Signup from './pages/SignupPages'
+import HomePages from './pages/HomePages'
+import TagSearchPages from './pages/TagSearchPages'
+import MyWebtoonPages from './pages/MyWebtoonPages'
+import MorePages from './pages/MorePages'
 import Welcome from './pages/WelcomePages'
 
+import RequireAuth from './components/common/RequireAuth'
+import Header from './components/common/header'
+import Navbar from './components/common/navbar'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* public routes */}
-        <Route path="/*" element={<HomePages />} />
-        <Route path="login" element={<LoginPages />} />
+    <>
+      <BrowserRouter>
+        <Header />
+        <Navbar />
+        <Routes>
+          {/* public routes */}
+          <Route path="login" element={<LoginPages />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="/*" element={<HomePages />} />
+          <Route path="tagsearch" element={<TagSearchPages />} />
+          <Route path="mywebtoon" element={<MyWebtoonPages />} />
+          <Route path="more" element={<MorePages />} />
 
-        {/* protected routes */}
-        <Route element={<RequireAuth />}>
-          <Route path="welcome" element={<Welcome />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          {/* protected routes */}
+            <Route path="welcome" element={<Welcome />} />
+          {/* <Route element={<RequireAuth />}>
+          </Route> */}
+
+        </Routes>
+      </BrowserRouter>
+
+    </>
   )
 }
 
