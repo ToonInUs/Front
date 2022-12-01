6d6e7960bda8925f2fcb472a10122465896c3416
Yerin <zlekfl0311@gmail.com>
2022-12-01 22:04:25 +0900

    #4 feat: 로그인 페이지 기본 틀 구현

--- src/pages/LoginPages/index.jsx
+++ src/pages/LoginPages/index.jsx
@@ -1,9 +1,11 @@
 import { useRef, useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-
+import { useNavigate, Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
+
 import { setCredentials } from '../../store/slices/authSlice'
 import { useLoginMutation } from '../../store/apis/authApiSlice'
+
+import PageTitle from '../../components/common/pageTitle'
 
 const Login = () => {
     const userRef = useRef()
@@ -57,34 +59,44 @@ const Login = () => {
             <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
 
             {/* title */}
-            <h1>로그인</h1>
+            {/* <h1>로그인</h1> */}
+            <PageTitle title="로그인" />
 
             <form onSubmit={handleSubmit}>
-                <label htmlFor="username">ID:</label>
                 <input
                     type="text"
                     id="username"
                     ref={userRef}
                     value={user}
-                    placeholder="이메일"
+                    placeholder="이메일 아이디"
                     onChange={handleUserInput}
                     autoComplete="off"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
                 />
 
-                <label htmlFor="password">PW:</label>
                 <input
                     type="password"
                     id="password"
                     value={pwd}
                     placeholder="비밀번호"
                     onChange={handlePwdInput}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
                 />
-                <button class="bg-black-900 hover:bg-black-800 active:bg-black-700 focus:outline-none focus:ring focus:ring-black-300 ...">
+                <button className="bg-gray-900 text-white hover:bg-gray-800 active:bg-gray-700 focus:outline-none focus:ring focus:ring-black-300">
                     로그인
                 </button>
+                {/* type="button" 으로 설정하면 submit을 막을 수 있다 */}
+                <button type="button" className="bg-gray-100 text-black hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:ring focus:ring-black-200">
+                    {/* block 속성으로 Link가 버튼 전체를 차지하도록함. 버튼 아무데나 눌러도 Link 동작 */}
+                    <Link to="/signup" className="block bg-gray-100 text-black hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:ring focus:ring-black-200">
+                        회원가입
+                    </Link>
+                </button>
+
             </form>
+
         </section>
     )
 
