42f9a5d565db1928d8b6ca55f72ff33afc6a8591
Yerin <zlekfl0311@gmail.com>
2022-12-06 01:00:13 +0900

    #4 design: navbar, pagetitle에 아이콘 추가. css font 추가

--- src/components/common/pageTitle.jsx
+++ src/components/common/pageTitle.jsx
@@ -5,10 +5,12 @@ const PageTitle = (props) => {
     return(
         <div className="border-b border-gray-300">
             <button onClick={()=>navigate(-1)} className="font-bold text-lg">
-                &lt; <label>{props.title}</label>
+                <span className="align-middle mr-3 material-symbols-outlined">arrow_back</span>
+                <label className="align-middle">{props.title}</label>
             </button>
         </div>
     )
 }
 
+// &lt;
 export default PageTitle
