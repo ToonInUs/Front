829eb2a6e34ad3ce7d18e0afb46370676bac0b1e
Yerin <zlekfl0311@gmail.com>
2022-12-14 17:41:44 +0900

    #23 feat: 좋아요 버튼 클릭하면 색 채워지도록 수정

---
+++ src/components/common/likeButton.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+const LikeButton = () => {
+    const [isLiked, setisLiked] = useState(false)
+    function likeToggler() {
+        {isLiked ?
+            setisLiked(false)
+            
+            : setisLiked(true)
+        }
+
+    }
+    return (
+        <button id="btn" onClick={likeToggler} className={(isLiked ? "text-red-400 " : "") + "inline ml-3 mr-2"}>
+            <span className="material-symbols-rounded" id={isLiked?"likeBtn":""}>favorite</span>
+        </button>
+    )
+}
+
+export default LikeButton
