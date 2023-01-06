e0307ceeaf679a1ebda802f1212ea7f130c343c9
Yerin <zlekfl0311@gmail.com>
2023-01-06 20:07:32 +0900

    #26 feat: my웹툰 페이지 tap 수정

--- src/pages/MyWebtoonPages/index.jsx
+++ src/pages/MyWebtoonPages/index.jsx
@@ -2,29 +2,44 @@ import React from 'react'
 import PageTitle from '../../components/common/PageTitle'
 import TabMywebtoon from '../../components/common/TabMywebtoon'
 import { Link } from 'react-router-dom'
+import Tag from '../../components/common/Tag'
+import TagSearchPages from '../TagSearchPages'
+import Button from '../../components/common/Button'
 
 // tailwind badge
 // https://flowbite.com/docs/components/badge/
 
 const MyWebtoonPages = () => {
+    const tags = [
+        { id: 0, content: '안녕' },
+        { id: 1, content: '반가워' },
+    ]
+
     return (
         <>
             <section>
                 <PageTitle title="My웹툰" />
                 <div className="content border-b border-gray-300">
-                    <div className="flex flex-row">
+                    <div className="flex flex-row items-center">
                         <label className="font-bold text-lg">취향태그 #</label>
-                        <a className="text-sm py-1 px-3 rounded-3xl ml-auto bg-gray-900 text-white hover:bg-gray-800 active:bg-gray-700 focus:outline-none focus:ring focus:ring-black-300">
-                            편집하기
-                        </a>
+                        <Button
+                            text={
+                                <>
+                                    <label className="align-middle text-sm text-gray-400">
+                                        편집하기
+                                    </label>
+                                    <span className="align-middle material-symbols-rounded text-sm text-gray-400">
+                                        arrow_forward_ios
+                                    </span>
+                                </>
+                            }
+                            link="/edittag"
+                        />
                     </div>
                     <div className="bg-gray-100 rounded-xl p-3">
-                        <Link
-                            to="/"
-                            className="bg-blue-100 hover:bg-blue-200 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 dark:hover:bg-blue-300"
-                        >
-                            #로맨스
-                        </Link>
+                        {tags.map((tag, index) => (
+                            <Tag tag={tag} key={index} />
+                        ))}
                     </div>
                 </div>
                 <div>
