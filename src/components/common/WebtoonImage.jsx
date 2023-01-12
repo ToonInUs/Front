88c31ed92a76bf601a7d8245299bcfac62bd2723
Yerin <zlekfl0311@gmail.com>
2023-01-12 22:44:34 +0900

    #26 design: LikeButton.jsx 수정

--- src/components/common/WebtoonImage.jsx
+++ src/components/common/WebtoonImage.jsx
@@ -3,11 +3,12 @@ import LikeButton from './LikeButton'
 
 const WebtoonImage = ({ webtoon }) => {
     return (
-        <div className="flex flex-col">
-            <Link to="/">
+        <div className="flex flex-col relative">
+            <Link to="/" className="relative">
                 <img className="rounded-xl" src="../../../img/poster.jpg" />
             </Link>
-            <LikeButton />
+            <LikeButton type="topRight" />
+            {/*  absolute top-0 right-0 */}
             <label className="text-sm">{webtoon.title}</label>
             <label className="text-sm text-gray-400">{webtoon.creator}</label>
         </div>
