59eebc263e1a35422a41dceeadbc34c946c7b9e4
Yerin <zlekfl0311@gmail.com>
2023-01-25 13:59:41 +0900

    #12 feat: categorySwiper 배열전달, WebtoonImage component 사용

--- src/components/common/CategorySwiper.jsx
+++ src/components/common/CategorySwiper.jsx
@@ -10,38 +10,28 @@ import 'swiper/css/pagination'
 
 // import required modules
 import { FreeMode, Pagination } from 'swiper'
+import WebtoonImage from './WebtoonImage'
 
-const CategorySwiper = (props) => {
+const CategorySwiper = ({ category, webtoons }) => {
     return (
         <>
             <div className="border-b border-gray-300">
-                <label className="text-lg font-bold">{props.category}</label>
+                <label className="text-lg">{category}</label>
             </div>
             <Swiper
                 slidesPerView={3.5}
-                spaceBetween={30}
+                spaceBetween={6}
                 freeMode={true}
                 modules={[FreeMode]}
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <Link className="flex flex-col" to="/">
-                        <img
-                            className="rounded-xl"
-                            src="../../../img/poster.jpg"
-                        />
-                        <label className="text-sm">이상한변호사우영우</label>
-                        <label className="text-sm text-gray-400">작가</label>
-                    </Link>
-                </SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+                {webtoons.map((webtoon, index) => (
+                    <SwiperSlide key={index}>
+                        <WebtoonImage webtoon={webtoon} />
+                        {/* <label className="text-sm">{webtoon.title}</label>
+                            <label className="text-sm text-gray-400">{webtoon.creator}</label> */}
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </>
     )
--- src/components/common/WebtoonImage.jsx
+++ src/components/common/WebtoonImage.jsx
@@ -3,14 +3,14 @@ import LikeButton from './LikeButton'
 
 const WebtoonImage = ({ webtoon }) => {
     return (
-        <div className="flex flex-col relative">
-            <Link to="/" className="relative">
-                <img className="rounded-xl" src="../../../img/poster.jpg" />
+        <div className="flex flex-col">
+            <Link to={`/webtoon/${webtoon.id}`} className="relative">
+                <img className="rounded-lg" src="../../../img/poster.jpg" />
             </Link>
             <LikeButton type="topRight" like={webtoon.like} />
             {/*  absolute top-0 right-0 */}
             <label className="text-sm">{webtoon.title}</label>
-            <label className="text-sm text-gray-400">{webtoon.creator}</label>
+            <label className="text-xs text-gray-300">{webtoon.creator}</label>
         </div>
     )
 }
