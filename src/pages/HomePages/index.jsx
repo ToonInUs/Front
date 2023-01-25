4bc8721d32b490219ad28003e2cbafd24374c707
Yerin <zlekfl0311@gmail.com>
2023-01-25 14:01:58 +0900

    #12 feat: CategorySwiper에 배열전달

--- src/pages/HomePages/index.jsx
+++ src/pages/HomePages/index.jsx
@@ -5,13 +5,21 @@ import TopSlider from '../../components/common/TopSlider'
 import CategorySwiper from '../../components/common/CategorySwiper'
 
 const HomePages = () => {
+    const webtoons = [
+        { title: '제목1', creator: '작가1', id: 0, like: true },
+        { title: '제목2', creator: '작가1', id: 1, like: true },
+        { title: '제목3', creator: '작가1', id: 2, like: false },
+        { title: '제목4', creator: '작가1', id: 3, like: true },
+        { title: '제목5', creator: '작가1', id: 4, like: false },
+        { title: '제목6', creator: '작가1', id: 5, like: true },
+    ]
     return (
         <div>
             {/* slider */}
             <div className="bg-black">
                 <TopSlider />
             </div>
-            {/* section ?? */}
+            {/* section */}
             <section className="content">
                 <Button
                     type="button"
@@ -19,10 +27,10 @@ const HomePages = () => {
                     color="dark"
                     link="/mywebtoon"
                 />
-                <CategorySwiper category="신작웹툰" />
-                <CategorySwiper category="인기웹툰" />
-                <CategorySwiper category="내 취향 추천" />
-                <CategorySwiper category="관심웹툰" />
+                <CategorySwiper category="신작웹툰" webtoons={webtoons} />
+                <CategorySwiper category="인기웹툰" webtoons={webtoons} />
+                <CategorySwiper category="내 취향 추천" webtoons={webtoons} />
+                <CategorySwiper category="관심웹툰" webtoons={webtoons} />
             </section>
         </div>
     )
